27d797bb313180a1a71586b06c4d752cff4f5c3a
zhanarbekabdurasulov <zhanarbek.a7@gmail.com>
2023-05-05 17:40:40 -0500

    Here I refreshed my webpage Made sleep after refresh page Then close browser

--- src/main/java/kg/codewise/testers/Main.java
+++ src/main/java/kg/codewise/testers/Main.java
@@ -4,15 +4,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         //https://github.com
         WebDriver chromeDriver = new ChromeDriver();
         chromeDriver.manage().window().maximize();
         String url = "https://www.google.com";
         chromeDriver.get(url);
-        chromeDriver.navigate().to("https://www.youtube.com");
-
+        chromeDriver.navigate().refresh();
+        Thread.sleep(10000);
+        chromeDriver.close();
 
     }
 }
