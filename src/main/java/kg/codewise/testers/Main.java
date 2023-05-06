fc18918b185340337a9c0ee8dc9a87817484874b
zhanarbekabdurasulov <zhanarbek.a7@gmail.com>
2023-05-05 22:15:20 -0500

    Here we have done so far: 1. Found elements as: usernameInput, passwordInput and loginButton input, click and write values.

--- src/main/java/kg/codewise/testers/Main.java
+++ src/main/java/kg/codewise/testers/Main.java
@@ -1,18 +1,42 @@
 package kg.codewise.testers;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
-        //https://github.com
         WebDriver chromeDriver = new ChromeDriver();
         chromeDriver.manage().window().maximize();
-        String url = "https://www.google.com";
+        String url = "https://www.saucedemo.com/";
         chromeDriver.get(url);
-        chromeDriver.navigate().refresh();
-        Thread.sleep(10000);
+
+        // to click on username input, and write username
+        WebElement usernameInput = chromeDriver
+                .findElement(By.xpath("//input[@id='user-name']"));
+        usernameInput.click();
+        usernameInput.sendKeys("standard_user");
+
+        Thread.sleep(5000);
+
+        // to click on password input, and write password
+        WebElement passwordInput = chromeDriver
+                .findElement(By.xpath("//input[@id='password']"));
+        passwordInput.click();
+        passwordInput.sendKeys("secret_sauce");
+
+        Thread.sleep(5000);
+
+
+        // to click on login button
+        WebElement loginButton = chromeDriver
+                .findElement(By.xpath("//input[@name='login-button']"));
+        loginButton.click();
+
+        Thread.sleep(5000);
+
         chromeDriver.close();
 
     }
