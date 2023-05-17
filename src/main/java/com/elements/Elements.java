ea6d66b23b4f70783bdc6f0f62a0e70f3d4a2e43
zhanarbekabdurasulov <zhanarbek.a7@gmail.com>
2023-05-17 18:58:52 -0500

    Little work with students

--- src/main/java/com/elements/Elements.java
+++ src/main/java/com/elements/Elements.java
@@ -2,12 +2,25 @@ package com.elements;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
 
 public class Elements {
     public static void main(String[] args)  {
-        readTable();
+        FirefoxOptions co = new FirefoxOptions();
+        co.addPreference("javascript.options.showInConsole", false);
+        WebDriver driver = new FirefoxDriver(co);
+        driver.get("https://tutorialsninja.com/demo/index.php?route=common/home");
+        WebElement imageOfIphone = driver.findElement(By.xpath("//img[@title='iPhone']"));
+        // click on some link
+        driver.findElement(By.linkText("Software")).click();
+        // go to page of iphone
+        imageOfIphone.click();
     }
 
     public static void readTable(){
@@ -34,8 +47,20 @@ public class Elements {
         }
     }
 
-    public static void otherElements(){
-
+    public static void interactWithSelect(){
+        System.setProperty("webdriver.firefox.logfile", "NUL");
+        FirefoxOptions co = new FirefoxOptions();
+        co.addPreference("javascript.options.showInConsole", false);
+        WebDriver driver = new FirefoxDriver(co);
+        driver.get("https://blazedemo.com/index.php");
+        WebElement selectElement = driver.findElement(By.name("fromPort"));
+        Select select = new Select(selectElement);
+        select.selectByIndex(select.getOptions().size()-1);
+//        select.selectByVisibleText("São Paolo");
+        List<WebElement> options = select.getOptions();
+        for (WebElement i : options) {
+            System.out.println("City: "+ i.getText()+" "+i.isSelected());
+        }
     }
 
 }
