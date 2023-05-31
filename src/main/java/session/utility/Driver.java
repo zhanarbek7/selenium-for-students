514e8e3d4319bb8f802a45c0df665457ab19dddf
zhanarbek <zhanarbek.a7@gmail.com>
2023-05-31 18:01:17 -0500

    finished Coronavirus

--- src/main/java/session/utility/Driver.java
+++ src/main/java/session/utility/Driver.java
@@ -1,29 +1,48 @@
 package session.utility;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 
-// This class we need to open browser and to close browser
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 public class Driver {
 
-    public static WebDriver driver;
+    private static WebDriver driver;
 
+    private static String browser = "chrome";
     public static WebDriver getDriver(){
-        if(driver==null){
-            WebDriverManager.firefoxdriver().arm64().setup();
-            FirefoxOptions co = new FirefoxOptions();
-            co.addPreference("javascript.options.showInConsole", false);
-            driver = new FirefoxDriver(co);
-            driver.manage().window().maximize();
+        if (driver == null){
+
+            if (browser.equalsIgnoreCase("chrome")){
+                driver = new ChromeDriver();
+                driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(50));
+                driver.manage().window().maximize();
+                return driver;
+            } else if (browser.equalsIgnoreCase("firefox")) {
+                driver = new FirefoxDriver();
+                driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
+                driver.manage().window().maximize();
+                return driver;
+            }
+            else{
+                System.out.println("Invalid Browser Type. Launching Default Browser");
+                driver = new ChromeDriver();
+                driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
+                driver.manage().window().maximize();
+                return driver;
+            }
+
+        }else {
+            return driver;
         }
-        return driver;
     }
 
-    public static void quit(){
-        driver.quit();
+    public static void quitDriver(){
+        if(driver!=null){
+            driver.quit();
+            driver=null;
+        }
     }
-
 }
