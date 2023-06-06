83b7320dff5b4cb8fc605752b31c0fa9ca361616
zhanarbek <zhanarbek.a7@gmail.com>
2023-06-06 10:06:53 -0500

    preparing for students

---
+++ src/test/java/utility/Driver.java
@@ -0,0 +1,50 @@
+package utility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+public class Driver {
+
+    private static WebDriver driver;
+
+    private static String browser = "chrome";
+    public static WebDriver getDriver(){
+        if (driver == null){
+
+            if (browser.equalsIgnoreCase("chrome")){
+                driver = new ChromeDriver();
+                driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(50));
+                driver.manage().window().maximize();
+                return driver;
+            } else if (browser.equalsIgnoreCase("firefox")) {
+                driver = new FirefoxDriver();
+                driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
+                driver.manage().window().maximize();
+                return driver;
+            }
+            else{
+                System.out.println("Invalid Browser Type. Launching Default Browser");
+                driver = new ChromeDriver();
+                driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
+                driver.manage().window().maximize();
+                return driver;
+            }
+
+        }else {
+            return driver;
+        }
+    }
+
+    public static void quitDriver(){
+        System.out.println("Quiting Google");
+        if(driver!=null){
+            driver.quit();
+            driver=null;
+        }
+    }
+}
