2873fb74fa7ffeec36929ef2877d77067e073a9a
zhanarbek <zhanarbek.a7@gmail.com>
2023-06-06 18:37:36 -0500

    I have done updateCourseName task

--- src/test/java/websites/HerokuSelenium.java
+++ src/test/java/websites/HerokuSelenium.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import session.utility.Driver;
+import utility.Driver;
 
 import java.time.Duration;
 
